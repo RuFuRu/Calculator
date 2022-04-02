0a62cbe42c8c57a2816623c33c92a288e949dfce
RuFuRu <94857865+RuFuRu@users.noreply.github.com>
2022-04-02 18:21:13 +0200

    adjustments to the support for digits

--- keyboard-support.js
+++ keyboard-support.js
@@ -1,10 +1,13 @@
 document.addEventListener('keydown',(e) => {
     display.setAttribute('style', 'border: 2px dotted rgb(37, 37, 37);');
-    //console.log(e.key);
-    const digitRegex = /[0-9]/
-    if(digitRegex.test(e.key)) {
-        display.textContent += e.key;
-    }
+
+    
+    const digit = ['0','1','2','3','4','5','6','7','8','9'];
+    digit.forEach(dig => {
+        if(e.key === dig) {
+            display.textContent += e.key;
+        }
+    })
 
     const operators = ['+','-','/','*','^'];
     operators.forEach(oper => {

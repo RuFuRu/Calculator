a05ef610b0a64d83d4db0bb70719566055a124fc
RuFuRu <94857865+RuFuRu@users.noreply.github.com>
2022-04-02 17:51:35 +0200

    added support for operators

--- keyboard-support.js
+++ keyboard-support.js
@@ -1,9 +1,22 @@
 document.addEventListener('keydown',(e) => {
     display.setAttribute('style', 'border: 2px dotted rgb(37, 37, 37);');
-    //console.log(typeof e.key);
-    let digitRegex = /[0-9]/
+    //console.log(e.key);
+    const digitRegex = /[0-9]/
     if(digitRegex.test(e.key)) {
         display.textContent += e.key;
+    }
+
+    const operators = ['+','-','/','*','^'];
+    operators.forEach(oper => {
+        if(e.key === oper) {
+            operator = e.key;
+            operatorList.push(operator);
+            evaluate();
+        }
+    })
+
+    if(e.key === '=') {
+
     }
 
 })
